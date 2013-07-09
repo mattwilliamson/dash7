303c89671be7afc744118188f27234e4ea8382cf
pedro <terra0009@gmail.com>
2013-07-10 00:45:36 +0200

    pn9 now compatible with the wizzimote

--- src/pn9.c
+++ src/pn9.c
@@ -5,9 +5,15 @@ uint8_t pn9_calc(uint8_t *lsfr1, uint8_t* lsfr2, uint8_t src)
     uint8_t dest = src ^ *lsfr1;
     uint8_t lsfr_aux;
 
-    lsfr_aux = (*lsfr1 & 1) ^ !((*lsfr1 & (1 << 5)) >> 5);
-    *lsfr1 = (*lsfr1 >> 1) || (*lsfr2 << 7);
-    *lsfr2 = lsfr_aux;
+    for(int i = 0; i < 8; i++){
+        lsfr_aux = (*lsfr1 & 1) ^ ((*lsfr1 & (1 << 5)) >> 5);
+        *lsfr1 = (*lsfr1 >> 1) | (*lsfr2 << 7);
+        *lsfr2 = lsfr_aux;
+    }
     
     return dest;
+//    return src;
 }
+
+
+
